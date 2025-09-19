a5e3058e44b6cfb3440a9e8ca54813c5e486a077
Daniel Smith <memorysight@gmail.com>
2025-09-19 10:13:44 -0400

    added productDTO, controller, service and impl plus logic

---
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -0,0 +1,21 @@
+package com.ecommerce.project.payload;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+    private Long id;
+    private String ProductName;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+
+}
