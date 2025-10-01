2d633e7c14a4641ac51dfa7753045ca44164e624
Daniel Smith <memorysight@gmail.com>
2025-10-01 11:13:40 -0400

    products pagination completed

--- src/main/java/com/ecommerce/project/payload/CategoryResponse.java
+++ src/main/java/com/ecommerce/project/payload/CategoryResponse.java
@@ -15,7 +15,7 @@ public class CategoryResponse {
     private Integer pageNumber;
     private Integer pageSize;
     private Long totalElements;
-    private Integer totalpages;
+    private Integer totalPages;
     private boolean lastPage;
 
 }
